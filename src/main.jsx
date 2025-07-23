914eefa8a3c0375fe42ab88e5e565097b3d39a4b
Ichiroki <fahrezirizqiawan12649@gmail.com>
2025-07-23 07:18:28 +0700

    try deploy with netlify

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./style.css"
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/my-portfolio'>
     <App />
   </BrowserRouter>
 )
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,9 +1,5 @@
 import ButtonLink from '../../component/ButtonLink';
 import './projects.css'
-import scythe from '/img/scythe.png'
-import swdm from '/img/swdm.png'
-import stockGudang from '/img/stock-gudang.png'
-import rapb from '/img/rapb.png'
 import { useLayoutEffect } from 'react'
 
 function Projects() {
@@ -24,56 +20,72 @@ function Projects() {
                                 textAlign: 'center'
                             }}
                         >
-                            I've been implement much of works that was originally created by me, this work either a web application or 3d Models. take a look.
+                            I've been implement much of works that was originally created by me, this work either a web application or 3d Models. take a look. Some of my project has link to show my original content, some of them doesn't have link or just show Github
                         </p>
-                        <div 
-                        style={{
-                            display: 'flex',
-                            flexWrap: 'wrap',
-                            alignItems: 'center',
-                            justifyContent: 'space-between',
-                            gap: '10px',
-                            marginTop: '25px'
-                        }}>
+                    </div>
+                    <div 
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'start',
+                        justifyContent: 'space-between',
+                        gap: '10px',
+                        marginTop: '25px'
+                    }}>
+                        <div>
+                            <h4 style={{
+                                textAlign: 'center',
+                                marginBottom: "10px",
+                                fontSize: "24px"
+                            }}><a href="https://skfb.ly/p8FxX" target='_blank'>1. Scythe of the dark doom</a></h4>
                             <div>
-                                <h4 style={{
-                                    textAlign: 'center',
-                                    marginBottom: "10px",
-                                    fontSize: "20px"
-                                }}>Scythe of the dark doom</h4>
-                                <img src={scythe} alt="" style={{
-                                    width: '200px'
-                                }}/>
+                                <span style={{display: 'flex'}}>Kind of Project : 3D Models</span>
+                                <ul style={{marginLeft: '1rem', marginTop: '15px'}}>
+                                    <li>Create using Blender</li>
+                                </ul>
                             </div>
+                        </div>
+                        <div>
+                        <h4 style={{
+                                textAlign: 'center',
+                                marginBottom: "10px",
+                                fontSize: "24px"
+                            }}><a href="https://swdm.me/" target='_blank'>2. Secret Weapon Deathmatch</a></h4>
                             <div>
-                                <h4 style={{
-                                    textAlign: 'center',
-                                    marginBottom: "10px",
-                                    fontSize: "20px"
-                                }}>Secret Weapon Deathmatch</h4>
-                                <img src={swdm} alt="" style={{
-                                    width: '250px'
-                                }}/>
+                                <span style={{display: 'flex'}}>Kind of Project : Website</span>
+                                <ul style={{marginLeft: '1rem', marginTop: '15px'}}>
+                                    <li>Designing user interface</li>
+                                    <li>Create rules page</li>
+                                </ul>
                             </div>
+                        </div>
+                        <div>
+                            <h4 style={{
+                                marginBottom: "10px",
+                                fontSize: "24px"
+                            }}><a href="https://swdm.me/" target='_blank'>3. Rencana Anggaran Penerimaan dan Beban</a></h4>
                             <div>
-                                <h4 style={{
-                                    textAlign: 'center',
-                                    marginBottom: "10px",
-                                    fontSize: "20px"
-                                }}>Stock Gudang</h4>
-                                <img src={stockGudang} alt="" style={{
-                                    width: '250px'
-                                }}/>
+                                <span style={{display: 'flex'}}>Kind of Project : Website</span>
+                                <ul style={{marginLeft: '1rem', marginTop: '15px'}}>
+                                    <li>Create Website using CodeIgniter4 and MySQL as a database</li>
+                                    <li>build a simple website application with adaptable user interface</li>
+                                    <li>has role and permission system</li>
+                                    <li>builded for API and Fullstack</li>
+                                </ul>
                             </div>
+                        </div>
+                        <div>
+                            <h4 style={{
+                                marginBottom: "10px",
+                                fontSize: "24px"
+                            }}><a href="https://ichiroki.itch.io/pixel-food-and-item-16-x-16" target='_blank'>4. Pixel Food and Item | 16 x 16</a></h4>
                             <div>
-                                <h4 style={{
-                                    textAlign: 'center',
-                                    marginBottom: "10px",
-                                    fontSize: "20px"
-                                }}>RAPB</h4>
-                                <img src={rapb} alt="" style={{
-                                    width: '250px'
-                                }}/>
+                                <span style={{display: 'flex'}}>Kind of Project : Design</span>
+                                <ul style={{marginLeft: '1rem', marginTop: '15px'}}>
+                                    <li>Create design using Aseprite</li>
+                                    <li>Created in 16 x 16 size</li>
+                                    <li>Has 70+ Design</li>
+                                </ul>
                             </div>
                         </div>
                     </div>
